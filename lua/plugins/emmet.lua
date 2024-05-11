5287e7cd95173230fe4e3823b02deff1e3dc4674
jason-zhang <gbyukg@163.com>
2024-05-11 22:40:55 +0800

    Add emmet support for HTML

---
+++ lua/plugins/emmet.lua
@@ -0,0 +1,7 @@
+return {
+  -- "olrtg/nvim-emmet",
+  -- config = function()
+  --   require("lspconfig").emmet_language_server.setup({})
+  --   vim.keymap.set({ "n", "v" }, "<leader>xe", require("nvim-emmet").wrap_with_abbreviation)
+  -- end,
+}
