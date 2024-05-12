41c1fee2aaa8d843aa820850575f32f30890f5ec
zz <gbyukg@163.com>
2024-05-12 10:18:56 +0800

    Change keymaps for file broser

--- lua/plugins/file-browser.lua
+++ lua/plugins/file-browser.lua
@@ -1,8 +1,32 @@
 return {
   "nvim-telescope/telescope-file-browser.nvim",
+  dependencies = {
+    {
+      "nvim-telescope/telescope.nvim",
+      opts = {
+        extensions = {
+          file_browser = {
+            theme = "ivy",
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+            -- mappings = {
+            --   ["i"] = {
+            --     -- your custom insert mode mappings
+            --     -- ["<TAB>"] = require("telescope").extensions.file_browser.actions.open, -- TODO
+            --   },
+            --   ["n"] = {
+            --     -- your custom normal mode mappings
+            --   },
+            -- },
+          },
+        },
+      },
+    },
+    "nvim-lua/plenary.nvim"
+  },
   keys = {
     {
-      "<leader>sB",
+      "<leader>.",
       ":Telescope file_browser path=%:p:h=%p:h<cr>",
       desc = "Browse Files",
     },
